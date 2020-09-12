d8b5ce17019b91d2d3d2a4fc2d1c8fa5f9e652ba
Jesse G <56103989+graffje@users.noreply.github.com>
2020-09-11 21:44:11 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -0,0 +1,39 @@
+var Alpha = 10;
+function Add_numbers_1() {
+    document.write(20 + Alpha + "<br>");
+}
+function Add_numbers_2() {
+    document.write(Alpha + 100 + "<br>");
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_3() {
+    var Beta = 10;
+    document.write(20 + Beta + "<br>");
+}
+function Add_numbers_4() {
+    document.write(Beta + 100 + "<br>");
+}
+Add_numbers_3
+Add_numbers_4
+
+var Gamma = 10;
+function Add_numbers_5() {
+    document.write(15 + Gamma + "<br>");
+}
+function Add_numbers_6() {
+    document.write(Gamma + 100 + "<br>")
+}
+Add_numbers_5();
+Add_numbers_6();
+
+if (5 > 4) {
+    document.write("The left number is larger than the right number.")
+}
+
+function get_Date() {
+    if (new Date().getHours() > 18) {
+    document.getElementById("Sign Off").innerHTML = "Night night!"
+    }
+}
